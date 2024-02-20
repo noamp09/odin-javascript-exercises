7799f41264e8a417fd12543c12b11a4116df5c85
noamp09 <noamp09@gmail.com>
2024-02-20 20:58:46 +0200

    finished removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(input, ...args) {
+    new_array = [];
+    input.forEach(element => {
+        if (!args.includes(element))
+        {
+            new_array.push(element);
+        }
+    });
+    return new_array;
 };
 
 // Do not edit below this line
