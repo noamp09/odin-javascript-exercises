6e919f908152d7433717f20c2529c7219b306429
noamp09 <noamp09@gmail.com>
2024-02-26 20:16:08 +0200

    finished fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,25 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    let count
+    if (typeof n !== 'number') {
+        count = parseInt(n)
+    } else {
+        count = n
+    }
 
+    if (count < 0) return "OOPS";
+    if (count == 0) return 0;
+
+    let firstPrev = 1;
+    let secondPrev = 0;
+
+    let result;
+    for (let i = 2; i <= count; i++) {
+        let current = firstPrev + secondPrev;
+        secondPrev = firstPrev;
+        firstPrev = current;
+    }
+
+    return firstPrev;
 };
 
 // Do not edit below this line
