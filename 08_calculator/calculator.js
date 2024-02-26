55d1a037bac93dc1824e60158a5453aabf36d81a
noamp09 <noamp09@gmail.com>
2024-02-26 19:56:38 +0200

    finished calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,22 @@
-const add = function() {
-	
-};
+const add = (a, b) => a + b;
 
-const subtract = function() {
-	
-};
+const subtract = (a, b) => a - b;
 
-const sum = function() {
-	
-};
+const sum = (arr) => arr.reduce((total, curr) => total + curr, 0);
 
-const multiply = function() {
+const multiply = (arr) => arr.reduce((total, curr) => total * curr, 1);
 
-};
+const power = (a, b) => Math.pow(a, b);
 
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function(num) {
+  if (num === 0) {
+    return 1
+  }
+  let result = 1;
+	for (let i = num; i > 0; i--){
+    result *= i;
+  }
+  return result
 };
 
 // Do not edit below this line
