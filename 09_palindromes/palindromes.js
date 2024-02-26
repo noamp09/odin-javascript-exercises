f7502a4bb5201350a0a8691dee8d9aed4a9f4681
noamp09 <noamp09@gmail.com>
2024-02-26 20:07:56 +0200

    finished palindrome

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,6 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    const result = str.toLowerCase().replace(/[^a-z0-9]/g, "");
+    return result.split("").reverse().join("") == result;
 };
 
 // Do not edit below this line
